3b0afa83e1f487e62968b8c96be40a235e8df42f
Rfed0r0v <rf.rf944@gmail.com>
2022-05-30 17:38:48 +0300

    Строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstName, middleName, lastName, fullName = "";
+        firstName = "Ivan";
+        middleName = "Ivanovich";
+        lastName = "Ivanov";
+        fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника -  " + fullName);
+        System.out.println("\nЗадание 2");
+        System.out.println("ФИО сотрудника для заполнения отчета -  " + fullName.toUpperCase());
+        System.out.println("\nЗадание 3");
+        System.out.println("ФИО сотрудника для административного отдела -  " + fullName.replace(" ","; "));
+        System.out.println("\nЗадание 4");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника -  " + fullName.replace("ё","е"));
+
+
+
     }
 }
